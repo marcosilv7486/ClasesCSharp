8b9f43ff2986aa6109cf282b2b5cfb47b6d9692f
Marco Silverio Castro <marcosilv7486@gmail.com>
2016-09-03 23:55:57 -0500

    Primmet commit
    Demostracion

---
+++ HolaMundo/HolaMundo/HolaMundo.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace HolaMundo
+{
+    partial class HolaMundo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(51, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Presioname!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(51, 46);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingrese su nombre";
+            // 
+            // HolaMundo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.button1);
+            this.Name = "HolaMundo";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.HolaMundo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ VariablesyConstantes/VariablesyConstantes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesyConstantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables  <- Son el lugar donde se almacena la informacion
+            // = asignacion de valores a mi variable
+            //Variables de tipo variente
+            var name = "Marcos Silverio Castro"; //<-Instruccion
+            var age = 24;
+            var phoneNumber = "012512518";
+            var pi = 3.141516;
+            //Variables de un solo tipo
+            int numberPets = 20;
+            double dollar = 3.14;
+            string lastname = "Silverio castro";
+            bool isMale = true;
+            //Ejercicios
+            int cantidadCel = 15;
+            //Declarar mis variables sin asignarle
+            int cantidadPersonajes;
+            cantidadPersonajes = 10;
+            cantidadPersonajes = 11;
+            cantidadPersonajes = 20;
+            int cantidadDeComputadoras;
+            cantidadDeComputadoras = 2;
+            cantidadPersonajes = cantidadDeComputadoras;
+            //Declarar constantes
+            const int gravedad = 10;
+            //imprimir en consola 
+            Console.WriteLine(lastname);
+            Console.ReadLine();
+
+        }
+    }
+}
